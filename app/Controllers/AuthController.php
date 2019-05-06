cc416c301ad5c0b28e186b8ab7a63d6241df291e
Aleksey Vasilenko <vasilenko@lanars.com>
2019-05-06 18:24:14 +0300

    added flash messages error

--- app/Controllers/AuthController.php
+++ app/Controllers/AuthController.php
@@ -8,7 +8,9 @@ class AuthController extends AbstractController
 {
     public function login($request, $response)
     {
-        return $this->view->render($response, 'auth/login.phtml');
+        $messages = $this->container->get('flash')->getMessages();
+
+        return $this->view->render($response, 'auth/login.phtml', $messages);
     }
 
     public function authenticate($request, $response, $args)
@@ -17,7 +19,30 @@ class AuthController extends AbstractController
 
         $data = $request->getParsedBody();
 
-        $auth->login($data['email'], $data['password']);
-        var_dump($data);
+        if($data['remember'] == 1) {
+            $rememberDuration = (int) (60 * 60 * 24 * 20);
+        }
+
+        try{
+            $auth->login($data['email'], $data['password'], $rememberDuration);
+            return $response->withRedirect('/profile');
+
+        }catch (\Delight\Auth\InvalidEmailException $e) {
+            $this->container->get('flash')->addMessage('login_error', 'Wrong email address');
+            return $response->withRedirect('/login');
+        }
+        catch (\Delight\Auth\InvalidPasswordException $e) {
+            $this->container->get('flash')->addMessage('login_error', 'Wrong password');
+            return $response->withRedirect('/login');
+        }
+        catch (\Delight\Auth\EmailNotVerifiedException $e) {
+            $this->container->get('flash')->addMessage('login_error', 'Email not verified');
+            return $response->withRedirect('/login');
+        }
+        catch (\Delight\Auth\TooManyRequestsException $e) {
+            $this->container->get('flash')->addMessage('login_error', 'Too many requests');
+            return $response->withRedirect('/login');
+        }
+
     }
 }
